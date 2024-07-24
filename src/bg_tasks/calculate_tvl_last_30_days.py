4092bd398bfdd453244d2b556886bff7362af2d6
trhuloc <trhuloc@gmail.com>
2024-07-24 15:53:33 -0500

    feat: Update calculate_tvl_last_30_days to include DAI contract address
    The `calculate_tvl_last_30_days` function now includes the DAI contract address in the code logic. This ensures that the correct deposit amount is calculated for transactions involving DAI.

--- src/bg_tasks/calculate_tvl_last_30_days.py
+++ src/bg_tasks/calculate_tvl_last_30_days.py
@@ -28,7 +28,8 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 session = Session(engine)
-DAI_CONTRACT_ADDRESS = "000000000000000000000000da10009cbd5d07dd0cecc66161fc93d7c9000da1"
+
+
 def calculate_tvl_last_30_days():
     logger.info("Starting calculate TVL last 30 days job...")
     now = datetime.now().timestamp()
@@ -43,37 +44,41 @@ def calculate_tvl_last_30_days():
         referrals = session.exec(statement).all()
         if len(referrals) == 0:
             continue
-        share_deposited =0
+        share_deposited = 0
         balance_deposited = 0
         share_withdraw = 0
         for referral in referrals:
-            statement  =select(User).where(User.user_id == referral.referee_id)
+            statement = select(User).where(User.user_id == referral.referee_id)
             referee = session.exec(statement).first()
-            statement = select(OnchainTransactionHistory).where(OnchainTransactionHistory.timestamp >= last_30_days_timestamp).where(OnchainTransactionHistory.from_address == referee.wallet_address)
+            statement = (
+                select(OnchainTransactionHistory)
+                .where(OnchainTransactionHistory.timestamp >= last_30_days_timestamp)
+                .where(OnchainTransactionHistory.from_address == referee.wallet_address)
+            )
             onchain_transaction_histories = session.exec(statement).all()
             if len(onchain_transaction_histories) == 0:
                 continue
             for onchain_transaction_historie in onchain_transaction_histories:
-                if onchain_transaction_historie.method_id == '0xb6b55f25':
-                    pps = get_pps_by_vault(onchain_transaction_historie,vaults)
+                if onchain_transaction_historie.method_id == "0xb6b55f25":
+                    pps = get_pps_by_vault(onchain_transaction_historie, vaults)
                     input = parse_hex_to_int(onchain_transaction_historie.input[10:])
-                    deposit = input/1e6
+                    deposit = input / 1e6
                     balance_deposited += deposit
                     share_deposited += deposit / pps
-                elif onchain_transaction_historie.method_id == '0x2e2d2984':
-                    pps = get_pps_by_vault(onchain_transaction_historie,vaults)
+                elif onchain_transaction_historie.method_id == "0x2e2d2984":
+                    pps = get_pps_by_vault(onchain_transaction_historie, vaults)
                     if pps is None:
                         continue
                     input = parse_hex_to_int(onchain_transaction_historie.input[10:74])
-                    if DAI_CONTRACT_ADDRESS in onchain_transaction_historie.input:
-                        deposit = input/1e18
+                    if constants.DAI_CONTRACT_ADDRESS in onchain_transaction_historie.input:
+                        deposit = input / 1e18
                     else:
-                        deposit = input/1e6
+                        deposit = input / 1e6
                     balance_deposited += deposit
                     share_deposited += deposit / pps
-                elif onchain_transaction_historie.method_id == '0x12edde5e':
+                elif onchain_transaction_historie.method_id == "0x12edde5e":
                     input = parse_hex_to_int(onchain_transaction_historie.input[10:])
-                    withdraw = input/1e6
+                    withdraw = input / 1e6
                     share_withdraw += withdraw
         if balance_deposited == 0:
             if share_deposited == 0:
@@ -89,22 +94,19 @@ def calculate_tvl_last_30_days():
             weighted_median_share=weighted_median_share,
             share_deposited=share_deposited,
             share_withdraw=share_withdraw,
-            total_value_locked=tvl
+            total_value_locked=tvl,
         )
         session.add(user_last_30_days_tvl)
         session.commit()
     logger.info("Calculate TVL last 30 days job completed.")
 
-def get_pps_by_vault(onchain_transaction_historie,vaults):
+
+def get_pps_by_vault(onchain_transaction_historie, vaults):
     for vault in vaults:
         if vault.contract_address.lower() == onchain_transaction_historie.to_address:
             vault_contract, w3 = get_vault_contract(vault)
-            pps = get_pps(vault_contract,onchain_transaction_historie.block_number)
+            pps = get_pps(vault_contract, onchain_transaction_historie.block_number)
             return pps
-
-
-
-
 
 
 if __name__ == "__main__":
