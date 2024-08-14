3778dcf33f19e341cc3dcb9b194168166308be45
Dung Bui <quangdung199697@gmail.com>
2024-08-14 11:03:32 +0700

    update vault

--- src/bg_tasks/calculate_yield_day_init.py
+++ src/bg_tasks/calculate_yield_day_init.py
@@ -2,7 +2,7 @@ import logging
 from datetime import datetime, timedelta, timezone
 from typing import List
 import uuid
-from sqlalchemy import func
+from sqlalchemy import and_, func
 from sqlmodel import Session, select
 
 from log import setup_logging_to_console, setup_logging_to_file
@@ -25,33 +25,53 @@ def get_active_vaults():
     return session.exec(select(Vault).where(Vault.is_active)).all()
 
 
-
-def get_vault_performances(vault_id, datetime: datetime):
-    return session.exec(
+def get_vault_performances(vault_id: uuid.UUID, date: datetime):
+    date_query = date.date()
+    data = session.exec(
         select(VaultPerformance)
         .where(VaultPerformance.vault_id == vault_id)
-        .where(VaultPerformance.datetime == datetime)
+        .where(func.date(VaultPerformance.datetime) == date_query)
         .order_by(VaultPerformance.datetime.asc())
     ).all()
+    return data
 
 
-def process_vault_performance(vault, datetime: datetime) -> float:
+def process_vault_performance_V2(vault, datetime: datetime) -> float:
     """Process performance for a given vault."""
-    vault_performances = get_vault_performances(vault.id, datetime)
-    current_tvl = sum(float(v.total_locked_value) for v in vault_performances)
 
-    previous_vault_performances = get_vault_performances(
+    current_tvl = 0
+    previous_vault_performances_tvl = 0
+    if vault.network_chain == constants.CHAIN_ETHER_MAINNET:
+        # Friday of weelky
+        friday_this_week = datetime - timedelta(days=(datetime.weekday() - 4))
+        vault_performances = get_vault_performances(vault.id, friday_this_week)
+        current_tvl = sum(float(v.total_locked_value) for v in vault_performances)
+        if datetime.weekday() == 4:
+            friday_last_week = friday_this_week - timedelta(days=7)
+            previous_vault_performances = get_vault_performances(
+                vault.id, friday_last_week
+            )
+            previous_vault_performances_tvl = sum(
+                float(v.total_locked_value) for v in previous_vault_performances
+            )
+    else:
+        vault_performances = get_vault_performances(vault.id, datetime)
+        current_tvl = sum(float(v.total_locked_value) for v in vault_performances)
+
+        previous_vault_performances = get_vault_performances(
         vault.id, datetime - timedelta(days=1)
-    )
-    
-    previous_vault_performances_tvl = sum(
+        )
+
+        previous_vault_performances_tvl = sum(
         float(v.total_locked_value) for v in previous_vault_performances
          )
-    
+
     tvl_change = current_tvl - previous_vault_performances_tvl
-    
+        
+    tvl_change = current_tvl - previous_vault_performances_tvl
+
     total_deposit = calculate_total_deposit(datetime.date())
-    return abs(tvl_change) - total_deposit
+    return tvl_change - total_deposit
 
 
 def calculate_total_deposit(vault_performance_date):
@@ -86,7 +106,8 @@ def get_vault_performance_dates() -> List[datetime]:
         date_list.append(current_date)
         current_date += timedelta(days=1)
 
-    return date_list   
+    return date_list
+
 
 def calculate_yield_init():
     logger.info("Starting calculate Yield init...")
@@ -96,16 +117,26 @@ def calculate_yield_init():
 
     for vault in vaults:
         # print('-----------------------valut------------------')
-        for vault_performance_date in vault_performance_dates:
-            yield_data = process_vault_performance(vault, vault_performance_date)
-            # if yield_data < 0:
-            #     print('sai meo r----------------------------')
-            insert_vault_performance_history(
-                yield_data=yield_data,
-                vault_id=vault.id,
-                datetime=vault_performance_date,
-            )
-        # print('***********************end----valut------------------')
+        if vault.network_chain == constants.CHAIN_ETHER_MAINNET:
+            for vault_performance_date in vault_performance_dates:
+                yield_data = process_vault_performance_V2(vault, vault_performance_date)
+                insert_vault_performance_history(
+                    yield_data=yield_data,
+                    vault_id=vault.id,
+                    datetime=vault_performance_date,
+                )
+        else:
+            # for vault_performance_date in vault_performance_dates:
+            #     yield_data = process_vault_performance(vault, vault_performance_date)
+            #     # if yield_data < 0:
+            #     #     print('sai meo r----------------------------')
+            #     insert_vault_performance_history(
+            #         yield_data=yield_data,
+            #         vault_id=vault.id,
+            #         datetime=vault_performance_date,
+            #     )
+            continue
+            # print('***********************end----valut------------------')
 
 
 if __name__ == "__main__":
