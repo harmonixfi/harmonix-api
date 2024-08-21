ba867f66a2a1024fdf347bd67b7264a425e65a84
baonguyen1904 <bao.nlq94@gmail.com>
2024-08-21 15:04:19 +0700

    adding health check endpoint

---
+++ src/api/api_v1/endpoints/healthz.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter, FastAPI, status
+from pydantic import BaseModel
+
+router = APIRouter()
+
+
+class HealthCheckResponse(BaseModel):
+    status: str
+
+
+@router.get(
+    "/healthz", response_model=HealthCheckResponse, status_code=status.HTTP_200_OK
+)
+async def health_check():
+    return HealthCheckResponse(status="ok")
