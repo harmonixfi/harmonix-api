655cf5270480b76dc1880104cf4fab6a1b15b7e0
Dung Bui <quangdung199697@gmail.com>
2024-08-25 13:38:09 +0700

    update data address

--- src/services/vault_performance_history_service.py
+++ src/services/vault_performance_history_service.py
@@ -73,6 +73,39 @@ class VaultPerformanceHistoryService:
         self.session.add(vault_performance_history)
         self.session.commit()
 
+    def get_vault_address_historical(self, vault: Vault) -> List[str]:
+        if (
+            vault.contract_address.lower()
+            == "0x4a10c31b642866d3a3df2268cecd2c5b14600523".lower()
+        ):
+            return [
+                "0x4a10c31b642866d3a3df2268cecd2c5b14600523",
+                "0xF30353335003E71b42a89314AAaeC437E7Bc8F0B",
+                "0x2b7cdad36a86fd05ac1680cdc42a0ea16804d80c",
+            ]
+
+        if (
+            vault.contract_address.lower()
+            == "0xd531d9212cB1f9d27F9239345186A6e9712D8876".lower()
+        ):
+            return [
+                "0x50CDDCBa6289d3334f7D40cF5d312E544576F0f9",
+                "0x607b19a600F2928FB4049d2c593794fB70aaf9aa",
+                "0xC9A079d7d1CF510a6dBa8dA8494745beaE7736E2",
+                "0x389b5702FA8bF92759d676036d1a90516C1ce0C4",
+                "0xd531d9212cB1f9d27F9239345186A6e9712D8876",
+            ]
+        if (
+            vault.contract_address.lower()
+            == "0x316CDbBEd9342A1109D967543F81FA6288eBC47D".lower()
+        ):
+            return [
+                vault.contract_address,
+                "0x0bD37D11e3A25B5BB0df366878b5D3f018c1B24c",
+            ]
+
+        return [vault.contract_address]
+
     def calculate_total_deposit(
         self,
         vault_performance_start_date: datetime,
@@ -83,14 +116,15 @@ class VaultPerformanceHistoryService:
         end_date = int(vault_performance_end_date.timestamp())
         start_date = int(vault_performance_start_date.timestamp())
 
+        contract_address = [
+            address.lower() for address in self.get_vault_address_historical(vault)
+        ]
         deposits_query = (
             select(OnchainTransactionHistory)
             .where(
                 OnchainTransactionHistory.method_id == constants.MethodID.DEPOSIT.value
             )
-            .where(
-                OnchainTransactionHistory.to_address == vault.contract_address.lower()
-            )
+            .where(OnchainTransactionHistory.to_address.in_(contract_address))
             .where(OnchainTransactionHistory.timestamp <= end_date)
             .where(OnchainTransactionHistory.timestamp >= start_date)
         )
