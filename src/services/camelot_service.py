975e9e465613d96638a9f7578a072449eeedaa62
Dung Bui <quangdung199697@gmail.com>
2024-09-16 16:32:28 +0700

    fix name param

--- src/services/camelot_service.py
+++ src/services/camelot_service.py
@@ -2,7 +2,7 @@ import requests
 from core.config import settings
 
 
-def get_pool_apy(params: str, chain_id: int = 42161) -> float:
+def get_pool_apy(pool_addres: str, chain_id: int = 42161) -> float:
     url = f"{settings.CAMELOT_EXCHANGE_API_URL}/v2/liquidity-v3-data?chainId={chain_id}"
     response = requests.get(url)
 
@@ -11,7 +11,7 @@ def get_pool_apy(params: str, chain_id: int = 42161) -> float:
 
     data = response.json()
 
-    apy_data = data["data"]["pools"][params]
+    apy_data = data["data"]["pools"][pool_addres]
     market_apr = round(float(apy_data["merkl"]["apr"]), 2)
     swap_fees_apr = round(float(apy_data["activeTvlAverageAPR"]), 2)
     return market_apr + swap_fees_apr
