970861c34b0cb1786ea285fef590706c17922121
Dung Bui <quangdung199697@gmail.com>
2024-09-30 07:11:24 +0700

    init model

---
+++ src/models/vault_rewards.py
@@ -0,0 +1,14 @@
+import uuid
+import sqlmodel
+
+
+class VaultRewards(sqlmodel.SQLModel):
+    __tablename__ = "vault_rewards"
+
+    id: uuid.UUID = sqlmodel.Field(default_factory=uuid.uuid4, primary_key=True)
+    vault_id: uuid.UUID
+    earned_rewards: float | None = None
+    unclaimed_rewards: float | None = None
+    claimed_rewards: float | None = None
+    token_address: str | None = None
+    token_name: int | None = None
