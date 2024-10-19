4328a1212f21f47146d7f82b23686c36c426fde5
Dung Bui <quangdung199697@gmail.com>
2024-10-19 21:58:25 +0700

    Implement background job to scan initiated withdrawal

---
+++ src/schemas/onchain_transaction_history.py
@@ -0,0 +1,14 @@
+import uuid
+from pydantic import BaseModel
+from datetime import datetime, time
+
+
+class OnchainTransactionHistory(BaseModel):
+    id: uuid.UUID
+    tx_hash: str
+    timestamp: int
+    datetime: datetime
+    method_id: str
+    input: str
+    amount: float = 0.0
+    age: time
