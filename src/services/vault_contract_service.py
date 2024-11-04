0c7d416a34c03ced339fe85e405464b2dbeaa9b4
Dung Bui <quangdung199697@gmail.com>
2024-11-04 09:37:21 +0700

    init check_deposit_withdraw_balance

--- src/services/vault_contract_service.py
+++ src/services/vault_contract_service.py
@@ -101,3 +101,32 @@ class VaultContractService:
             ]
 
         return [vault.contract_address.lower()]
+
+    def get_vault_address_by_contract(self, contract_address: str) -> List[str]:
+        contract_address = contract_address.lower()
+        vault_addresses = {
+            "0x4a10c31b642866d3a3df2268cecd2c5b14600523": [
+                "0x4a10c31b642866d3a3df2268cecd2c5b14600523",
+                "0xF30353335003E71b42a89314AAaeC437E7Bc8F0B",
+                "0x2b7cdad36a86fd05ac1680cdc42a0ea16804d80c",
+            ],
+            "0xd531d9212cb1f9d27f9239345186a6e9712d8876": [
+                "0x50CDDCBa6289d3334f7D40cF5d312E544576F0f9",
+                "0x607b19a600F2928FB4049d2c593794fB70aaf9aa",
+                "0xC9A079d7d1CF510a6dBa8dA8494745beaE7736E2",
+                "0x389b5702FA8bF92759d676036d1a90516C1ce0C4",
+                "0xd531d9212cb1f9d27f9239345186a6e9712d8876",
+            ],
+            "0x316cdbbed9342a1109d967543f81fa6288ebc47d": [
+                "0x316cdbbed9342a1109d967543f81fa6288ebc47d",
+                "0x0bD37D11e3A25B5BB0df366878b5D3f018c1B24c",
+                "0x18994527E6FfE7e91F1873eCA53e900CE0D0f276",
+                "0x55c4c840F9Ac2e62eFa3f12BaBa1B57A1208B6F5",
+            ],
+        }
+
+        for addresses in vault_addresses.values():
+            if contract_address in addresses:
+                return addresses
+
+        return [contract_address]
